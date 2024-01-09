898b894691b31d0aeda8cde104c9fcd326886390
dukekunyura <dukekunyura@gmail.com>
2024-01-09 17:03:09 +0300

    [+] add index one

---
+++ index-one.js
@@ -0,0 +1,58 @@
+// let undefined = 1;
+
+// console.log(undefined);
+
+// let two;
+
+// console.log(two === undefined);
+// console.log(two);
+
+const similer = (a, b) => {
+  for (key in a) {
+    if (a[key] !== b[key]) {
+      if (typeof a[key] === "object") {
+        return similer(a[key], b[key]);
+      }
+      return false;
+    }
+  }
+  return true;
+};
+
+const objA = {
+  name: "one",
+  surname: "two",
+  age: 5,
+  gender: false,
+  child: {
+    one: "one",
+  },
+};
+
+global.objB = {
+  name: "one",
+  surname: "two",
+  age: 5,
+  gender: false,
+  child: {
+    one: "one1",
+  },
+  funOne: () => {
+    return this;
+  },
+  getName(args) {
+    const into = (...args) => {
+      return { thisOf: this, args: args[0] };
+    };
+    return into(args);
+  },
+};
+
+console.log(objA === objB);
+console.log(similer(objA, objB));
+
+// console.log(objA);
+// console.log({ objA });
+
+console.log(objB.funOne());
+console.log(objB.getName(888));
