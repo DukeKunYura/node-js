85b03d1960b67533a1d56e90f87a8d6dcfc44c5c
dukekunyura <dukekunyura@gmail.com>
2024-01-11 16:21:23 +0300

    [+] add index three

--- index-three.js
+++ index-three.js
@@ -1,3 +1,4 @@
+// копирование массива в массив и в объект
 items = [
   { itemOne: "one" },
   { itemTwo: "two", itemThree: { one: 1, two: 2, tree: { three: 3 } } },
@@ -11,3 +12,29 @@ items[0]["itemTwo"] = "two";
 
 console.log(one);
 console.dir(two, { depth: null });
+
+// множества и хешмапы
+
+const setItems = new Set(items);
+console.log(setItems);
+
+const objOne = { name: "name", surname: "surname" };
+// const arrOne = Object.entries(objOne);
+// console.log(arrOne);
+
+const mapItems = new Map();
+let i = 0;
+items.forEach((item) => {
+  mapItems.set(i, item);
+  i++;
+});
+console.log(mapItems);
+
+const mapItemsTwo = new Map([
+  ...Object.entries(items[0]),
+  ...Object.entries(items[1]),
+]);
+console.log(mapItemsTwo);
+
+// цвета в консоли
+console.log("\x1b[1;33m" + 5555 + "\t" + "\x1b[1;35m" + 1 + "\x1b[1;37m");
